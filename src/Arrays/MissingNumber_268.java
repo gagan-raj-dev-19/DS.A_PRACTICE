8710d3e3ccfabcd5c27dc25a07953b94d430f17d
Gagan raj <gaganrajba@gmail.com>
2024-12-18 22:57:47 +0530

    Add solution for Missing Number - LeetCode - 268

---
+++ src/Arrays/MissingNumber_268.java
@@ -0,0 +1,17 @@
+package Arrays;
+//class MissingNumber {
+//public static int missingNum(int[] nums){
+//    int res = nums.length;
+//    for (int i =0;i<nums.length;i++){
+//        res = res+ i - nums[i];
+//    }
+//    return res;
+//}
+//
+//    public static void main(String[] args) {
+//        int[] nums = {1,2,3,0,4};
+//        int res = missingNum(nums);
+//        System.out.println(res);
+//    }
+//}
+
