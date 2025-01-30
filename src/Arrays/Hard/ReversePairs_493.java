3e700e7476512e73b296abc503e960baea7cabb6
Gagan raj <gaganrajba@gmail.com>
2025-01-30 15:19:56 +0530

    Add solution for REVERSE PAIRS - LeetCode - 493.

---
+++ src/Arrays/Hard/ReversePairs_493.java
@@ -0,0 +1,37 @@
+package Arrays.Hard;
+
+
+class ReversePairs {
+    public static int reversePairs(int[] nums){
+
+
+    }
+}
+
+
+
+
+
+// bruteforce O(n^2) and O(1)  TLE -> LeetCode
+// class ReversePairs {
+//     public static int reversePairs(int[] nums){
+//         int n = nums.length;
+//         int count = 0;
+//         for (int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i] > 2*nums[j]){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+//
+//     public static void main(String[] args) {
+//         int[] nums= {1,3,2,3,1};
+//         int[] nums1= {2,4,3,5,1};
+//         System.out.println(reversePairs(nums));
+//         System.out.println(reversePairs(nums1));
+//
+//     }
+//}
