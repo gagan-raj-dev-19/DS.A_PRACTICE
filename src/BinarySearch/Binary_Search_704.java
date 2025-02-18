4c6a07d2c16587900944ce4fec260270bec5ad20
Gagan raj <gaganrajba@gmail.com>
2025-02-18 19:33:59 +0530

    Add solution for Binary Search - LeetCode - 704

--- src/BinarySearch/Binary_Search_704.java
+++ src/BinarySearch/Binary_Search_704.java
@@ -1,8 +1,9 @@
 package BinarySearch;
-
-class Solution {
+// iterative approach
+class Iterative {
     public int search(int[] nums, int target) {
         int n = nums.length;
+
         int left =0;
         int right = n-1;
 
@@ -17,5 +18,42 @@ class Solution {
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        Iterative obj = new Iterative();
+        int[] nums = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        System.out.println("iterative o/p = "+obj.search(nums,target));
+    }
+}
+
+// recursive approach
+
+class Recursive {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0,nums.length-1,target);
+    }
+
+    public static int binarySearch(int[]nums, int left, int right, int target){
+        if(right < left){
+            return -1;
+        }
+        int mid = left + (right-left)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            return binarySearch(nums,mid+1,right,target);
+        }else{
+            return binarySearch(nums,left,mid-1,target);
+        }
+    }
+
+    public static void main(String[] args) {
+        Recursive obj1 = new Recursive();
+        int[] nums= {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        System.out.println("recursive o/p = "+obj1.search(nums,target));
     }
 }
