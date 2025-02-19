293ac9b7436d7a654f425a94192005afeb31b530
Gagan raj <gaganrajba@gmail.com>
2025-02-19 14:40:33 +0530

    Add solution for Search in Rotated Sorted Array - LeetCode- 33.

--- src/BinarySearch/Medium/SearchinRotatedSortedArray_33.java
+++ src/BinarySearch/Medium/SearchinRotatedSortedArray_33.java
@@ -29,7 +29,7 @@ package BinarySearch.Medium;
 
      public static void main(String[] args) {
          int[] arr = {4,5,6,7,0,1,2};
-         int target=5;
+         int target=0;
          System.out.println(searchRotatedArray(arr,target));
      }
 }
