d9ad37e72c2347064e44b2ea31ca6b172d392490
Gagan raj <gaganrajba@gmail.com>
2025-02-24 13:56:07 +0530

    Add solution for Binary Search - LeetCode- 704

--- src/BinarySearch/Easy/Binary_Search_704.java
+++ src/BinarySearch/Easy/Binary_Search_704.java
@@ -1,59 +1,26 @@
 package BinarySearch.Easy;
-// iterative approach
-class Iterative {
-    public int search(int[] nums, int target) {
-        int n = nums.length;
 
-        int left =0;
-        int right = n-1;
+// bruteforce linear search approach
 
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(target < nums[mid]){
-                right = mid-1;
-            }else{
-                left = mid+1;
+class LinearSearch{
+    public static int search(int[] arr, int target){
+        for(int i=0;i< arr.length;i++){
+            if(arr[i] == target){
+                return i;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        Iterative obj = new Iterative();
-        int[] nums = {3, 4, 6, 7, 9, 12, 16, 17};
-        int target = 6;
-        System.out.println("iterative o/p = "+obj.search(nums,target));
+        int [] arr = {1,3,5,7,9,11};
+        int target = 7;
+        int result = search(arr,target);
+        System.out.println(result);
     }
 }
+// TC -> O(n)
+// SC -> O(1)
 
-// recursive approach
 
-class Recursive {
-    public int search(int[] nums, int target) {
-        return binarySearch(nums, 0,nums.length-1,target);
-    }
 
-    public static int binarySearch(int[]nums, int left, int right, int target){
-        if(right < left){
-            return -1;
-        }
-        int mid = left + (right-left)/2;
-        if(nums[mid] == target){
-            return mid;
-        }
-        else if(target > nums[mid]){
-            return binarySearch(nums,mid+1,right,target);
-        }else{
-            return binarySearch(nums,left,mid-1,target);
-        }
-    }
-
-    public static void main(String[] args) {
-        Recursive obj1 = new Recursive();
-        int[] nums= {3, 4, 6, 7, 9, 12, 16, 17};
-        int target = 6;
-        System.out.println("recursive o/p = "+obj1.search(nums,target));
-    }
-}
