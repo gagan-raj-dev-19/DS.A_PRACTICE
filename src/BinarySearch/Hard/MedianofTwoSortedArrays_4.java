721886164bd7de1ac37c23f71f7ead3107e39c0b
Gagan raj <gaganrajba@gmail.com>
2025-05-21 15:31:54 +0530

    Add solution for Median of Two Sorted Arrays - LeetCode - 4

--- src/BinarySearch/Hard/MedianofTwoSortedArrays_4.java
+++ src/BinarySearch/Hard/MedianofTwoSortedArrays_4.java
@@ -31,8 +31,5 @@ class MedianofTwoSortedArrays {
     public static void main(String[] args) {
         int[] nums1 = {1,2};
         int[] nums2 = {3,4};
-        System.out.println(findMedianSortedArrays(nums1,nums2));
-    }
-}
-
-// pending
+        System.out.println(findMedianSortedArrays(nums1,nums2
+//
