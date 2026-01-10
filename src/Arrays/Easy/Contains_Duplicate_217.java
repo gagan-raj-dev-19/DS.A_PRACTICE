deca5de14db1389fb332e2a71cba0fadbe98d4c4
Gagan raj <gaganrajba@gmail.com>
2026-01-10 23:07:32 +0530

    Update Contains_Duplicate_217

---
+++ src/Arrays/Easy/Contains_Duplicate_217.java
@@ -0,0 +1,25 @@
+package Arrays.Easy;
+
+import java.util.HashSet;
+
+public class Contains_Duplicate_217 {
+    public static boolean containsDuplicate(int[] nums){
+        HashSet<Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(containsDuplicate(nums));
+
+
+    }
+
+}
